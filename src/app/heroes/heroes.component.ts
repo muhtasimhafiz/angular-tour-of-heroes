6fd3a3d4c870ea2dfc941437ff4ae5f7c604c6c8
DESKTOP-SVBVC1U\HP <muhtasimhafiz@gmail.com>
2024-04-28 22:09:25 +0600

    chapter 2

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,17 +2,19 @@ import { Component } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
-
+import { HEROES } from '../mock-heroes';
+// import { NgFor } from '@angular/common';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  standalone:true,
-  imports:[CommonModule, FormsModule]
+  standalone: true,
+  imports: [CommonModule, FormsModule],
 })
 export class HeroesComponent {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
